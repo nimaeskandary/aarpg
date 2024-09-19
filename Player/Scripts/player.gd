177a19248fb313e8a761abecf7192748a5bbc8b9
Nima Eskandary <nimaesk1@gmail.com>
2024-09-18 20:34:11 -0400

    vid 2, player idle and walk animations

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,6 +1,12 @@
 class_name Player extends CharacterBody2D
 
+var cardinal_direction: Vector2 = Vector2.DOWN
+var direction: Vector2 = Vector2.ZERO
 var move_speed: float = 100.0
+var state: String = "idle"
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,11 +15,50 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var direction: Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	velocity = direction * move_speed
+	
+	if set_state() || set_direction():
+		update_animation()
 
 
 func _physics_process(delta):
 	move_and_slide()
+
+
+func set_direction() -> bool:
+	var new_direction: Vector2 = cardinal_direction
+	if direction == Vector2.ZERO:
+		return false
+	elif direction.y == 0:
+		new_direction = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_direction = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	if new_direction == cardinal_direction:
+		return false
+	cardinal_direction = new_direction
+	# scaling instead of setting flip horizontal flag so that child nodes also are flipped
+	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
+	return true
+
+
+func set_state() -> bool:
+	var new_state: String = "idle" if direction == Vector2.ZERO else "walk"
+	if new_state == state:
+		return false
+	state = new_state
+	return true
+	
+
+func update_animation() -> void:
+	animation_player.play(state + "_" + anim_direction())
+
+
+func anim_direction() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
