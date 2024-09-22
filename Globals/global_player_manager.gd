40013966d6fe993c1a5d0a5b18327e09e83b9dd3
Nima Eskandary <nimaesk1@gmail.com>
2024-09-22 19:10:55 -0400

    vid 14, player spawner

--- Globals/global_player_manager.gd
+++ Globals/global_player_manager.gd
@@ -1,3 +1,31 @@
 extends Node
 
+const PLAYER: PackedScene = preload("res://Player/player.tscn")
 var player: Player
+var player_spawned: bool  = false
+
+
+func _ready() -> void:
+	add_player_instance()
+	# failsafe in case we forgot to add a player spawn
+	await get_tree().create_timer(0.2).timeout
+	player_spawned = true
+
+
+func add_player_instance() -> void:
+	player = PLAYER.instantiate()
+	add_child(player)
+
+
+func set_player_position(new_pos: Vector2) -> void:
+	player.global_position = new_pos
+
+
+func set_as_parent(p: Node2D) -> void:
+	if player.get_parent():
+		player.get_parent().remove_child(player)
+	p.add_child(player)
+
+
+func unparent_player(p: Node2D) -> void:
+	p.remove_child(player)
